323c9ade600df50de6efdf4e922f5994f06bebf8
Rehan Jaffer <ray@thelondonvandal.com>
2018-01-21 18:04:11 +0000

    added definitions for redcode

---
+++ redcode.h
@@ -0,0 +1,20 @@
+#define DAT 0
+#define MOV 1
+#define ADD 2
+#define SUB 3
+#define MUL 4
+#define DIV 5
+#define MOD 6
+#define JMP 7
+#define JMZ 8
+#define JMN 9
+#define DJN 10
+#define SPL 11
+#define CMP 12
+#define SEQ 13
+#define SNE 14
+#define SLT 15
+#define LDP 16
+#define STP 17
+#define NOP 18
+
