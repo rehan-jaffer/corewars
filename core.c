e849f72acd0aa1d5787c829c71bacdd51e225324
Rehan Jaffer <ray@thelondonvandal.com>
2018-02-24 13:07:18 +0000

    attempting to add ability to alternate execution of programs

--- core.c
+++ core.c
@@ -2,11 +2,14 @@
 
 #define CORE_SIZE 4096
 #define CORE_WIDTH 64
+#define DEBUG 1
 
 struct core {
   uint32_t cells[CORE_SIZE];
   uint32_t p1_start;
   uint32_t p2_start;
+  uint32_t ip1;
+  uint32_t ip2;
 };
 
 struct program {
@@ -49,7 +52,52 @@ void exec_core(struct core *c, struct program p1, struct program p2) {
   memcpy(&c->cells[p2_loc], &p2.instructions, sizeof(p2.instructions));
   c->p1_start = p1_loc;
   c->p2_start = p2_loc;
-  print_core(c);
+  c->ip1 = 0;
+  c->ip2 = 0;
+  uint8_t cur_program = 0;
+
+  for (int x=0;x<100;x++) {
+
+    int ip = 0;
+
+    if (x % 2 == 0) {
+      cur_program = 1;
+      ip = c->ip1;
+      if (DEBUG) {
+        printf("Executing Program #1 at %d", c->ip1);
+      }
+    } else {
+      cur_program = 2;
+      ip = c->ip2;
+      if (DEBUG) {
+        printf("Executing Program #2 at %d", c->ip1);
+      }
+    }
+
+
+    switch(c->cells[ip]) {
+      case 0:
+        printf("NOP\r\n");
+      break;
+    }
+
+    switch(cur_program) {
+      case 1:
+        c->ip1++;
+      break;
+      case 2:
+        c->ip2++;
+      break;
+    }
+
+  }
+
+  //print_core(c);
+
+  uint32_t cycles = 0;
+
+  
+
   return 0;
 
 }
